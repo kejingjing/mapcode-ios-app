21db2a2810d1b26d5a5d2d94862b7a683dae80e9
Rijn Buve <rijn@buve.nl>
2017-08-30 11:51:10 +0200

    Updated

--- Mapcode/ViewController.swift
+++ Mapcode/ViewController.swift
@@ -20,6 +20,7 @@ import CoreLocation
 import MapKit
 import UIKit
 import Contacts
+
 // FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
 // Consider refactoring the code to use the non-optional operators.
 fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
